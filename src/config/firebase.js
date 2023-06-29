a3ad7b56d9abbffbd406a886adc8998efb2a4b2c
Prasath <prasathkarthiban1999@gmail.com>
2023-06-29 16:10:32 +0530

    test

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,19 +2,24 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-require("dotenv").config();
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
+  apiKey: "AIzaSyC26s-erEHbGDnYOW96PfWZ3AHNU6gSxtE",
+  authDomain: "project-alpha-phase-1.firebaseapp.com",
+  projectId: "project-alpha-phase-1",
+  storageBucket: "project-alpha-phase-1.appspot.com",
+  messagingSenderId: "624596079081",
+  appId: "1:624596079081:web:868e9ece885e3cd8257b78",
+  measurementId: "G-RR0BRX6YXS",
+  // apiKey: process.env.API_KEY,
+  // authDomain: process.env.AUTH_DOMAIN,
+  // projectId: process.env.PROJECT_ID,
+  // storageBucket: process.env.STORAGE_BUCKET,
+  // messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  // appId: process.env.APP_ID,
+  // measurementId: process.env.MEASUREMENT_ID,
 };
 
-console.log(firebaseConfig);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
